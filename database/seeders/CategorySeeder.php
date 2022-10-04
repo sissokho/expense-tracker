edc39c95f8789344054420a21ae9919559c69813
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-10-04 14:48:39 +0000

    Set up database table and seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        Category::factory()
+            ->count(23)
+            ->create();
+    }
+}
