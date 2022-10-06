fa281210f973f47cfcf55a49ae115451fa83dbc5
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-10-06 15:21:37 +0000

    Setup livewire hot reloading with vitejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import { livewire_hot_reload } from "virtual:livewire-hot-reload";
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+livewire_hot_reload();
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+import livewire from "@defstudio/vite-livewire-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            // refresh: [
+            //     ...refreshPaths,
+            //     'app/Http/Livewire/**',
+            // ],
+            refresh: false,
+        }),
+        livewire({
+            refresh: ["resources/css/app.css"],
         }),
     ],
 });
