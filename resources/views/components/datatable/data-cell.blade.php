783a742cb39dd34d3619837b10bddec9c287d51c
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-11-01 12:11:19 +0000

    Show top 10 expenses/incomes

--- resources/views/components/datatable/data-cell.blade.php
+++ resources/views/components/datatable/data-cell.blade.php
@@ -1,3 +1,3 @@
-<td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+<td scope="row" {{ $attributes->merge(['class' => 'py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white']) }}>
     {{ $slot }}
 </td>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden p-5 space-y-10 sm:rounded-lg">
                 <livewire:dashboard.stats-cards />
+
+                <div class="flex flex-col items-center gap-5 lg:flex-row lg:justify-between">
+                    <livewire:dashboard.top-transactions :type="\App\Enums\TransactionType::Income" />
+                    <livewire:dashboard.top-transactions :type="\App\Enums\TransactionType::Expense" />
+                </div>
             </div>
         </div>
     </div>
